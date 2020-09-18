bbec8bfa8735295fbd762bf0f2ce92e7cd7c5598
srtabarestarquinio <49605681+srtabarestarquinio@users.noreply.github.com>
2020-09-18 11:23:39 -0500

    Update KLists.java

--- KLists.java
+++ KLists.java
@@ -1,1 +1,14 @@
+import java.util.*;
 
+public class KLists{
+  
+  
+  public static void main(String[] args){
+    KLists lists = new KLists();
+
+		Scanner input=new Scanner(System.in);
+  }
+  
+  
+  
+}
